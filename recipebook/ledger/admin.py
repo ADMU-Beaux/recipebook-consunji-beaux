7b12ea4af84468a3d1526143f98c02ba0e3c1f62
ADMU-Beaux <beaux.consunji@student.ateneo.edu>
2024-03-15 15:53:25 +0800

    Modify templates and admin page

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient, Ingredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Recipe, RecipeIngredient, Ingredient, Profile
+
+# User models
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+# Recipe models
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
 
 class RecipeAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_on', 'updated_on', )
     inlines = [RecipeIngredientInline, ]
 
 class IngredientAdmin(admin.ModelAdmin):
