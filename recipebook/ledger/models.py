602f0ce1eaa51a8332d65b837662b808de3d80e5
ADMU-Beaux <beaux.consunji@student.ateneo.edu>
2024-03-15 15:42:15 +0800

    Create models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse
 
-# Create your models here.
+# User models
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio = models.CharField(max_length=255)
+
+# Recipe models
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
@@ -14,7 +21,10 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now = True)
+    
     def __str__(self):
         return str(self.name)
 
@@ -25,3 +35,4 @@ class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+
